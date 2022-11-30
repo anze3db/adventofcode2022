7fe796cac1ea11524bcf2b69f5b1e1f3746d8e2f
Anže Pečar <anze@pecar.me>
2022-11-30 11:09:53 +0000

    Prepare for day 01

---
+++ 01.py
@@ -0,0 +1,11 @@
+from utils import console, get_input, submit
+
+inp = get_input(1)
+console.log(inp)
+
+res = 1
+assert 2 == 2
+submit(day=1, level=1, answer=res)
+
+assert 1 == 2
+submit(day=1, level=2, answer=res)
