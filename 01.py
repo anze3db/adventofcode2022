28cc3e99b49a366dc809796cf962c9e4d2104a70
Anže Pečar <anze@pecar.me>
2022-12-01 08:20:18 +0000

    Solve day 01

--- 01.py
+++ 01.py
@@ -3,9 +3,16 @@ from utils import console, get_input, submit
 inp = get_input(1)
 console.log(inp)
 
-res = 1
-assert 2 == 2
-submit(day=1, level=1, answer=res)
+s = 0
+elves = []
+for i in inp:
+    if i == "":
+        elves.append(s)
+        s = 0
+        continue
+    s += int(i)
+res = max(elves)
+# submit(day=1, level=1, answer=res)
 
-assert 1 == 2
-submit(day=1, level=2, answer=res)
+res = sum(sorted(elves)[-3:])
+# submit(day=1, level=2, answer=res)
